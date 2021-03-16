9ed378a04bb5305566fb781bf3395ac38a4f8fde
fatimasilvaed <fatimasilvaed@gmail.com>
2021-03-16 00:18:06 -0300

    create dict

--- cadastroPessoas.py
+++ cadastroPessoas.py
@@ -1,25 +1,38 @@
 import json
 
-def loadjsonfile():
+def carrregajsonparaarquivo():
 
-    with open("cadastro_pessoas.json") as file:
-        jsonfile = json.load(file)
+    with open("cadastro_pessoas.json") as arquivo:
+        arquivojson = json.load(arquivo)
 
-        return jsonfile
+        return arquivojson
 
 def createpeople():
 
     pessoa = {}
+    telefone = {}
 
     pessoa['cpf'] = input("Digite cpf:\n")
     pessoa['nome'] = input("Digite nome:\n")
-    pessoa[]
+    pessoa['idade'] = input("Qual idade:\n")
+    telefone['ddd'] = input("Digite DDD\n")
+    telefone['Número'] = input("Digite Telefone:\n")
+
+    pessoa['telefone'] = telefone
 
     print(pessoa)
 
     return pessoa
 
-createpeople()
+def loadpeopletojson(listapessoas):
+
+    with open("cadastro_pessoas.json") as arquivojson:
+       arquivodump = json.dumps(listadepessoas)
+        arquivojson.write(arquivodump)
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+from cadastroPessoas import carrregajsonparaarquivo,createpeople,loadpeopletojson
 
+
+def createnewperson():
+
+    lista = carrregajsonparaarquivo()
+    novapessoa = createpeople()
+    print(novapessoa)
+    lista.append(novapessoa)
+    lista = str(lista)
+    print(lista)
+    loadpeopletojson(lista)
+
+createnewperson()
 
