c0a87ff798a30911f5fe165769dea71de2da4b6d
fatimasilvaed <fatimasilvaed@gmail.com>
2021-03-21 16:52:04 -0300

    pessoa dict add to json file ok

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from cadastroPessoas import carrregajsonparaarquivo,createpeople,loadpeopletojson
 from buscar_pessoa import buscarpessoaporcpf
+from deleta_pessoa import deletarpessoa
 import json
 
 
 def selecionarfuncao():
 
-    func = input("SELECIONE:\n"
+    func = int(input("SELECIONE:\n"
                  "1 - NOVO CADASTRO\n"
                  "2 - BUSCAR CADASTRO\n"
-                 "3 - EXLCUIR CADASTRO\n")
+                 "3 - EXLCUIR CADASTRO\n"))
 
     return func
 
@@ -17,12 +18,19 @@ def cadastropessoas():
     funcao = selecionarfuncao()
 
     if funcao == 1:
-
         lista = carrregajsonparaarquivo()
         novapessoa = createpeople()
         lista.append(novapessoa)
         loadpeopletojson(lista)
 
+    elif funcao == 2:
+        buscarpessoaporcpf()
+
+    elif funcao == 3:
+        deletarpessoa()
+
+
+cadastropessoas()
 
 
 
