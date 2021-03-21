b988c1a73d6ec2ad47f11169cc35646fc19b19a9
fatimasilvaed <fatimasilvaed@gmail.com>
2021-03-21 16:52:04 -0300

    pessoa dict add to json file ok

---
+++ deleta_pessoa.py
@@ -0,0 +1,24 @@
+import json
+
+def deletarpessoa():
+
+    cpfdelete = str(input("Digite CPF para Excluir:\n"))
+
+    with open("cadastro_pessoas.json") as arquivo:
+        jsonpessoa = json.load(arquivo)
+
+    for pessoa in jsonpessoa:
+
+        if cpfdelete == pessoa.get("cpf"):
+            confirmacao = int(input("Deseja excluir {}\n 1 - SIM \n 2 - NÃO\n".format(pessoa)))
+
+            if confirmacao == 1:
+                jsonpessoa.remove(pessoa)
+                print("CADASTRO DELETADO COM SUCESSO !\n")
+
+    with open("cadastro_pessoas.json",'w') as arquivojson:
+
+        arquivodump = json.dumps(jsonpessoa)
+        arquivojson.write(arquivodump)
+
+
